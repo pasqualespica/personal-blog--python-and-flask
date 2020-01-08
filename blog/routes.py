1b6651014a3c518b211caee2035a3bcc963a95b7
pasqualespica <pasqualespica>
2020-01-08 10:09:04 +0100

    first brick

--- blog/routes.py
+++ blog/routes.py
@@ -9,14 +9,17 @@ from blog import app
 @app.route("/")
 def homepage():
     # return "<h1>Hello BLOG!<h1>"
-    posts = [ {"title" : "primo posrt", "body" : "random body1"},
-              {"title": "secondo posrt", "body": "random body2"}, ]
+    posts = [ {"title" : "primo post", "body" : "random body1"},
+              {"title": "secondo post", "body": "random body2"}, ]
 
     some_bool_flag = False
-
 
     # passare queste variabili come "context" dei nostri templates
     return render_template("homepage.html", 
                             posts=posts, 
                             boolean_fg=some_bool_flag)
+
+@app.route("/about")
+def about():
+    return render_template('about_page.html')
 
